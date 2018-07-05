47cd39fd444ce6527db65b2294639acc5cb892a1
MacBook Pro <macbookpro@MacBook-Pro-de-MacBook.local>
2018-07-06 00:44:23 +0200

    Finalisation du formulaire d'inscription

--- Flaskblog.py
+++ Flaskblog.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'f334629b7e141f50f5edb664e038ccf4'
 
 posts = [
 
@@ -29,6 +33,21 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html",title='About')
+
+@app.route("/register", methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+    	flash(f'Account created for { form.username.data }!','success')
+    	return redirect(url_for('home'))
+
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+
 
 
 if __name__ == '__main__':
