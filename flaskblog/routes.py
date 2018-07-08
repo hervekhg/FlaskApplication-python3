943c707aa3ce4d7238e43ca32e2c3ea5c468c82d
MacBook Pro <macbookpro@MacBook-Pro-de-MacBook.local>
2018-07-09 01:27:13 +0200

    Finalisation de l'authentification

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,8 +1,8 @@
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
-from flask_login import login_user, current_user, logout_user
+from flask_login import login_user, current_user, logout_user, login_required
 
 posts = [
 
@@ -61,7 +61,8 @@ def login():
       user = User.query.filter_by(email=form.email.data).first()
       if user and bcrypt.check_password_hash(user.password, form.password.data):
         login_user(user, remember=form.remember.data)
-        return redirect(url_for('home'))
+        next_page = request.args.get('next')
+        return redirect(next_page) if next_page else redirect(url_for('home'))
       else:
         flash('Login unsuccesful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
@@ -71,3 +72,8 @@ def login():
 def logout():
 	logout_user()
 	return redirect(url_for('home'))
+
+@app.route("/account")
+@login_required
+def account():
+	return render_template('Account.html', title='Account')
