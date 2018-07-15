765be4bb9fcbb058333309583f83b780386e861d
MacBook Pro <macbookpro@MacBook-Pro-de-MacBook.local>
2018-07-15 20:53:00 +0200

    Update & Delete post with modal

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from flask_login import current_user
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User 
 
@@ -64,3 +64,9 @@ class UpdateAccountForm(FlaskForm):
 			user = User.query.filter_by(email=email.data).first()
 			if user:
 				raise ValidationError('That email is already taken. Please choose another')
+
+
+class PostForm(FlaskForm):
+	title = StringField('Title',validators=[DataRequired()])
+	content = TextAreaField('Content',validators=[DataRequired()])
+	submit = SubmitField('Post')
