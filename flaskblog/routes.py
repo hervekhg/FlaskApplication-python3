7c79348f7cf235acf060873019e50caff31bd097
MacBook Pro <macbookpro@MacBook-Pro-de-MacBook.local>
2018-07-15 21:52:26 +0200

    Page des posts de chaque user

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -11,9 +11,8 @@ from flask_login import login_user, current_user, logout_user, login_required
 @app.route("/")
 @app.route("/home")
 def home():
-    #posts = Post.query.all()
-    page = request.args.get('page',1, type=int)
-    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=3)
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template('home.html', posts=posts)
 
 
@@ -104,39 +103,53 @@ def new_post():
         db.session.commit()
         flash('Your post has been created!', 'success')
         return redirect(url_for('home'))
-    return render_template('create_post.html', title='New Post',form=form, legend='New Post')
+    return render_template('create_post.html', title='New Post',
+                           form=form, legend='New Post')
+
 
 @app.route("/post/<int:post_id>")
 def post(post_id):
     post = Post.query.get_or_404(post_id)
     return render_template('post.html', title=post.title, post=post)
 
+
 @app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
 @login_required
 def update_post(post_id):
     post = Post.query.get_or_404(post_id)
-    if post.author !=  current_user:
-    	abort(403)
+    if post.author != current_user:
+        abort(403)
     form = PostForm()
     if form.validate_on_submit():
-    	post.title = form.title.data
-    	post.content = form.content.data
-    	db.session.commit()
-    	flash('Your post has been Updated!', 'success')
-    	return redirect(url_for('post', post_id=post.id))
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('post', post_id=post.id))
     elif request.method == 'GET':
-    	form.title.data = post.title
-    	form.content.data = post.content
-    post = Post(title=form.title.data, content=form.content.data, author=current_user)
-    return render_template('create_post.html', title='Update Post',form=form, legend='Update Post')
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post',
+                           form=form, legend='Update Post')
+
 
 @app.route("/post/<int:post_id>/delete", methods=['POST'])
 @login_required
 def delete_post(post_id):
     post = Post.query.get_or_404(post_id)
-    if post.author !=  current_user:
-    	abort(403)
+    if post.author != current_user:
+        abort(403)
     db.session.delete(post)
     db.session.commit()
-    flash('Your post has been Deleted!', 'success')
+    flash('Your post has been deleted!', 'success')
     return redirect(url_for('home'))
+
+
+@app.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(page=page, per_page=5)
+    return render_template('user_posts.html', posts=posts, user=user)
