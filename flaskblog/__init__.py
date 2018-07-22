832c6f6fd81661cd7e9f31dc40fb515d23edaf95
MacBook Pro <macbookpro@MacBook-Pro-de-MacBook.local>
2018-07-22 16:32:48 +0200

    Reset Password and Send Email through GMAIL - End

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,8 +1,10 @@
+import os
 from datetime import datetime
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 
 
@@ -14,5 +16,12 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+
+mail = Mail(app)
 
 from flaskblog import routes
--- flaskblog/models.py
+++ flaskblog/models.py
@@ -20,13 +20,13 @@ class User(db.Model, UserMixin):
 	def __repr__(self):
 		return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
-	def get_reset_token(expires_sec=1800):
+	def get_reset_token(self, expires_sec=1800):
 		s = Serializer(app.config['SECRET_KEY'], expires_sec)
 		return s.dumps({'user_id': self.id}).decode('utf-8')
 
 	@staticmethod
-	def verify_reset_tokey(self,token):
-		s = Serializer(app.config['SECRET_KEY'], expires_sec)
+	def verify_reset_token(token):
+		s = Serializer(app.config['SECRET_KEY'])
 		try:
 			user_id = s.loads(token)['user_id']
 		except Exception as e:
