078c856f176c3ac4f3804ca9d0b0311d04b96f88
MacBook Pro <macbookpro@MacBook-Pro-de-MacBook.local>
2018-07-26 00:42:55 +0200

    Mise a jour de la page about

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -7,36 +6,30 @@ from flask_mail import Mail
 from flaskblog.config import Config
 
 
-
-
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
-
-
 mail = Mail()
 
 
-
 def create_app(config_class=Config):
-	app = Flask(__name__)
-	app.config.from_object(Config)
+    app = Flask(__name__)
+    app.config.from_object(Config)
 
-	db.init_app(app)
-	bcrypt.init_app(app)
-	login_manager.init_app(app)
-	mail.init_app(app)
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
 
-	from flaskblog.users.routes import users
-	from flaskblog.posts.routes import posts
-	from flaskblog.main.routes import main
-	from flaskblog.errors.handlers import errors
-	app.register_blueprint(users)
-	app.register_blueprint(posts)
-	app.register_blueprint(main)
-	app.register_blueprint(errors)
+    from flaskblog.users.routes import users
+    from flaskblog.posts.routes import posts
+    from flaskblog.main.routes import main
+    from flaskblog.errors.handlers import errors
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+    app.register_blueprint(errors)
 
-
-	return app
+    return app
--- run.py
+++ run.py
@@ -2,5 +2,6 @@
 from flaskblog import create_app
 
 app = create_app()
+
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
