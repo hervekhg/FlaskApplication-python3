a85f4cf6f78a9098e6417952cda71346ef941534
MacBook Pro <macbookpro@MacBook-Pro-de-MacBook.local>
2018-07-30 01:36:53 +0200

    modification du formattage du mail pour le rendre compatible avec la version python du serveur

--- flaskblog/users/utils.py
+++ flaskblog/users/utils.py
@@ -35,10 +35,10 @@ def send_reset_email(user):
     msg = Message('Password Reset Request',
                   sender='noreply@demo.com',
                   recipients=[user.email])
-    msg.body = f'''To reset your password, visit the following link:
-{url_for('users.reset_token', token=token, _external=True)}
+    msg.body = '''To reset your password, visit the following link:
+{%s}
 If you did not make this request then simply ignore this email and no changes will be made.
-'''
+''' %(url_for('users.reset_token', token=token, _external=True))
     mail.send(msg)
 
 
