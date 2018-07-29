510e57425e1edfe86323a2f88b48da21c6a4d296
MacBook Pro <macbookpro@MacBook-Pro-de-MacBook.local>
2018-07-30 01:16:04 +0200

    Changement serveur d'envoi de mail

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -3,8 +3,10 @@ import os
 class Config:
 	SECRET_KEY = 'f334629b7e141f50f5edb664e038ccf4'
 	SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-	MAIL_SERVER = 'smtp.googlemail.com'
-	MAIL_PORT = 587
+	#MAIL_SERVER = 'smtp.googlemail.com'
+	#MAIL_PORT = 587
+	MAIL_SERVER = 'localhost'
+	MAIL_PORT = 25
 	MAIL_USE_TLS = True
 	MAIL_USERNAME = os.environ.get('EMAIL_USER')
 	MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
