253b58576f4db105eacb20a39f3a1930702a0339
MacBook Pro <macbookpro@MacBook-Pro-de-MacBook.local>
2018-08-17 15:36:39 +0200

    Suppression des heures dans la sitemap pour google

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -22,6 +22,7 @@ def sitemap_xml():
 	"""Generate sitemap.xml. Makes a list of urls and date modified."""
 	pages=[]
 	ten_days_ago=datetime.now() - timedelta(days=10)
+	ten_days_ago=ten_days_ago.strftime('%Y-%m-%d')
 	# static pages
 	# for rule in current_app.url_map.iter_rules():
 	# 	if "GET" in rule.methods and len(rule.arguments)==0:
