34754970b3bfbae3f7af33d37f710c007f498193
MacBook Pro <macbookpro@MacBook-Pro-de-MacBook.local>
2018-08-19 21:34:59 +0200

    Ajout du bug sur l'erreur de validation du formulaire de création des story

--- flaskblog/posts/forms.py
+++ flaskblog/posts/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
+from wtforms import StringField, SubmitField, TextAreaField, ValidationError
 from wtforms.validators import DataRequired
 
 class PostForm(FlaskForm):
